93d03676e21f19853eeb633c362a0229a0533cbc
Mishal Adnan <mishal.adnan26@gmail.com>
2023-08-01 00:06:22 +0500

    Updates

---
+++ precache-manifest.558318e61cf3ac9ec797918d22192af9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d20f007aa8b429d72af8",
+    "url": "/folio/static/css/main.f04769c2.chunk.css"
+  },
+  {
+    "revision": "d20f007aa8b429d72af8",
+    "url": "/folio/static/js/main.d20f007a.chunk.js"
+  },
+  {
+    "revision": "09b7ccdffa0112bf4e37",
+    "url": "/folio/static/js/1.09b7ccdf.chunk.js"
+  },
+  {
+    "revision": "cc4d656b426a302234ff",
+    "url": "/folio/static/js/2.cc4d656b.chunk.js"
+  },
+  {
+    "revision": "b4ea6b8b0b5d42820688",
+    "url": "/folio/static/js/runtime~main.b4ea6b8b.js"
+  },
+  {
+    "revision": "f14f0ba612aad699a57386671ba848b4",
+    "url": "/folio/index.html"
+  }
+];
